7c7e8046a743ac0f480e8e562499b16582e970fc
Nixxo <92588172+Nekuso@users.noreply.github.com>
2022-06-01 04:28:35 +0800

    added files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector('.nav__hamburger');
+const navmenu = document.querySelector('.nav__menu')
+const navlogo = document.querySelector('.nav__logo')
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('is-active');
+    navmenu.classList.toggle('is-active');
+    navlogo.classList.toggle('is-active');
+
+});
