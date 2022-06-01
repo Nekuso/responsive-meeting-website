18fd3b616f6d474165f71d1b4cace0a97feea1b7
Nixxo <92588172+Nekuso@users.noreply.github.com>
2022-06-02 03:05:40 +0800

    added files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Hamburger Menu
 const hamburger = document.querySelector('.nav__hamburger');
 const navmenu = document.querySelector('.nav__menu');
 const navlogo = document.querySelector('.nav__logo');
@@ -7,4 +8,13 @@ hamburger.addEventListener('click', function() {
     navmenu.classList.toggle('is-active');
     navlogo.classList.toggle('is-active');
 
+});
+
+// Dark Mode Toggle
+const darkmode = document.querySelector('.dark__mode__button');
+const body = document.body;
+
+darkmode.addEventListener('click', function() {
+    darkmode.classList.toggle('is-dark');
+    body.classList.toggle('is-dark');
 });
